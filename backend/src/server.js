fabedb92c810b288ff87c563cb66d8dab46a5ff3
harshsingla123 <harshsingla339@gmail.com>
2026-08-22 19:36:39 +0530

    deployconfig added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
+const __dirname = path.resolve();
 
 const app = express();
 console.log(ENV.PORT);
 app.get("/health", (req, res) => {
    res.status(200).json({ message: "api is running fine" });
 }); 
+if(ENV.NODE_ENV === "production") {
+   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+   app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+   });
+}
 app.listen(ENV.PORT, () => {
    console.log("Server is running on port " + ENV.PORT);
 }); 
