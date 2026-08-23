48ec3ad27876abda1b9fff3fdfe52ed00c4fe2c3
harshsingla123 <harshsingla339@gmail.com>
2026-08-23 19:38:18 +0530

    auth sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,26 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
+import {serve} from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
+
 const __dirname = path.resolve();
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
+app.use("/api/inngest",serve({clint:inngest,functions}))
+
 console.log(ENV.PORT);
 app.get("/health", (req, res) => {
    res.status(200).json({ message: "api is running fine" });
+});
+app.get("/books", (req, res) => {
+   res.status(200).json({ message: "This is the books endpoint" });
 });
 if (ENV.NODE_ENV === "production") {
    app.use(express.static(path.join(__dirname, "../frontend/dist")));
